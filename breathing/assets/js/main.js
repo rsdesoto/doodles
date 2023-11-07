29d5d7d8507fee101e4949f944f2de99b8f281c2
Theo <ry.e.desoto@gmail.com>
2023-11-07 08:52:29 -0600

    Removing console logs

--- breathing/assets/js/main.js
+++ breathing/assets/js/main.js
@@ -4,11 +4,9 @@ const pauseButton = document.getElementById('pause-button');
 const animation = document.querySelector(".circle")
 
 playButton.addEventListener('click', () => {
-    console.log("play");
     animation.style.animationPlayState = 'running';
 })
 
 pauseButton.addEventListener('click', () => {
-    console.log("pause");
     animation.style.animationPlayState = 'paused';
 })
